4acca5d2f3f0d67035ffc2e2ba20d2411d66179f
Muhammed Firat Ozturk <firatdmf@gmail.com>
2023-01-22 17:38:25 -0800

    few more changes on windows

--- frontend/src/components/FabricDisplay.jsx
+++ frontend/src/components/FabricDisplay.jsx
@@ -1,88 +1,127 @@
 import classes from "./FabricDisplay.module.css";
 function FabricDisplay() {
-
-    function imageZoom(imgID, resultID) {
-        var img, lens, result, cx, cy;
-        img = document.getElementById(imgID);
-        result = document.getElementById(resultID);
-        /* Create lens: */
-        lens = document.createElement("DIV");
-        lens.setAttribute("class", "img-zoom-lens");
-        /* Insert lens: */
-        img.parentElement.insertBefore(lens, img);
-        /* Calculate the ratio between result DIV and lens: */
-        cx = result.offsetWidth / lens.offsetWidth;
-        cy = result.offsetHeight / lens.offsetHeight;
-        /* Set background properties for the result DIV */
-        result.style.backgroundImage = "url('" + img.src + "')";
-        result.style.backgroundSize = (img.width * cx) + "px " + (img.height * cy) + "px";
-        /* Execute a function when someone moves the cursor over the image, or the lens: */
-        lens.addEventListener("mousemove", moveLens);
-        img.addEventListener("mousemove", moveLens);
-        /* And also for touch screens: */
-        lens.addEventListener("touchmove", moveLens);
-        img.addEventListener("touchmove", moveLens);
-        function moveLens(e) {
-          var pos, x, y;
-          /* Prevent any other actions that may occur when moving over the image */
-          e.preventDefault();
-          /* Get the cursor's x and y positions: */
-          pos = getCursorPos(e);
-          /* Calculate the position of the lens: */
-          x = pos.x - (lens.offsetWidth / 2);
-          y = pos.y - (lens.offsetHeight / 2);
-          /* Prevent the lens from being positioned outside the image: */
-          if (x > img.width - lens.offsetWidth) {x = img.width - lens.offsetWidth;}
-          if (x < 0) {x = 0;}
-          if (y > img.height - lens.offsetHeight) {y = img.height - lens.offsetHeight;}
-          if (y < 0) {y = 0;}
-          /* Set the position of the lens: */
-          lens.style.left = x + "px";
-          lens.style.top = y + "px";
-          /* Display what the lens "sees": */
-          result.style.backgroundPosition = "-" + (x * cx) + "px -" + (y * cy) + "px";
-        }
-        function getCursorPos(e) {
-          var a, x = 0, y = 0;
-          e = e || window.event;
-          /* Get the x and y positions of the image: */
-          a = img.getBoundingClientRect();
-          /* Calculate the cursor's x and y coordinates, relative to the image: */
-          x = e.pageX - a.left;
-          y = e.pageY - a.top;
-          /* Consider any page scrolling: */
-          x = x - window.pageXOffset;
-          y = y - window.pageYOffset;
-          return {x : x, y : y};
-        }
+  function imageZoom(imgID, resultID) {
+    var img, lens, result, cx, cy;
+    img = document.getElementById(imgID);
+    result = document.getElementById(resultID);
+    /* Create lens: */
+    lens = document.createElement("DIV");
+    lens.setAttribute("class", "img-zoom-lens");
+    /* Insert lens: */
+    img.parentElement.insertBefore(lens, img);
+    /* Calculate the ratio between result DIV and lens: */
+    cx = result.offsetWidth / lens.offsetWidth;
+    cy = result.offsetHeight / lens.offsetHeight;
+    /* Set background properties for the result DIV */
+    result.style.backgroundImage = "url('" + img.src + "')";
+    result.style.backgroundSize =
+      img.width * cx + "px " + img.height * cy + "px";
+    /* Execute a function when someone moves the cursor over the image, or the lens: */
+    lens.addEventListener("mousemove", moveLens);
+    img.addEventListener("mousemove", moveLens);
+    /* And also for touch screens: */
+    lens.addEventListener("touchmove", moveLens);
+    img.addEventListener("touchmove", moveLens);
+    function moveLens(e) {
+      var pos, x, y;
+      /* Prevent any other actions that may occur when moving over the image */
+      e.preventDefault();
+      /* Get the cursor's x and y positions: */
+      pos = getCursorPos(e);
+      /* Calculate the position of the lens: */
+      x = pos.x - lens.offsetWidth / 2;
+      y = pos.y - lens.offsetHeight / 2;
+      /* Prevent the lens from being positioned outside the image: */
+      if (x > img.width - lens.offsetWidth) {
+        x = img.width - lens.offsetWidth;
       }
+      if (x < 0) {
+        x = 0;
+      }
+      if (y > img.height - lens.offsetHeight) {
+        y = img.height - lens.offsetHeight;
+      }
+      if (y < 0) {
+        y = 0;
+      }
+      /* Set the position of the lens: */
+      lens.style.left = x + "px";
+      lens.style.top = y + "px";
+      /* Display what the lens "sees": */
+      result.style.backgroundPosition = "-" + x * cx + "px -" + y * cy + "px";
+    }
+    function getCursorPos(e) {
+      var a,
+        x = 0,
+        y = 0;
+      e = e || window.event;
+      /* Get the x and y positions of the image: */
+      a = img.getBoundingClientRect();
+      /* Calculate the cursor's x and y coordinates, relative to the image: */
+      x = e.pageX - a.left;
+      y = e.pageY - a.top;
+      /* Consider any page scrolling: */
+      x = x - window.pageXOffset;
+      y = y - window.pageYOffset;
+      return { x: x, y: y };
+    }
+  }
+  let imgUrl = "images/fabrics/1002.jpg";
+  let width = 120;
+  let application = "Drapery";
+  let unit = "inches";
+  let origin = "Turkey";
+  let type = "Sheer";
+  let theme ='Classic';
+  let SKU = 'K12471';
+  let Name = SKU;
+  return (
+    <>
+      <div className={classes.container}>
+        <div className={classes.image}>
+          <img src={imgUrl} alt="" />
+        </div>
+        <div className={classes.separator}></div>
+        <div className={classes.info}>
+          <h1>{SKU}</h1>
+          <ul>
+            <li>
+              <b>SKU: </b>
+              {SKU}
+            </li>
 
-    let imgUrl = "https://www.thecreativecurator.com/wp-content/uploads/2021/01/knit-fabric-2-1024x653.jpg";
-    let width = 120;
-    let application = 'Drapery';
-    let unit = 'inches';
-    return (
-        <>
-            <div className={classes.container}>
-                <div className={classes.image}>
-                    <img src={imgUrl} alt="" />
-                </div>
-                <div className={classes.separator}></div>
-                <div className={classes.info}>
-                    <h1>NAME</h1>
-                    <ul>
-                        <li><b>SKU: </b>{'K12471'}</li>
-                        
-                        <li className={classes.application}><b>Application: </b>{application}</li>
-                        {(application!=='Drapery') && (<li><b>Double Rubs: </b>{'50,000'}</li>)}
-                        
-                        <li><b>Content:</b>{'Polypropylene'}</li>
-                        <li><b>Width: </b>{width+' ' + unit}</li>
-                    </ul>
-                </div>
-            </div>
-        </>
-    )
+            <li className={classes.application}>
+              <b>Application: </b>
+              {application}
+            </li>
+            {application !== "Drapery" && (
+              <li>
+                <b>Double Rubs: </b>
+                {"50,000"}
+              </li>
+            )}
+            <li>
+              <b>Type: </b>
+              {type}
+            </li>
+            <li><b>Theme: </b>{theme}</li>
+            <li>
+              <b>Content: </b>
+              {"Polypropylene"}
+            </li>
+            <li>
+              <b>Width: </b>
+              {width + " " + unit}
+            </li>
+            <li>
+              <b>Origin: </b>
+              {origin}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default FabricDisplay
+export default FabricDisplay;
