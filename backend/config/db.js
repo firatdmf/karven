fdd10ee98a3e4673b83e11b54ab23ae03ee5eec9
Muhammed Firat Ozturk <firatdmf@gmail.com>
2023-02-24 19:13:15 -0800

    nice looking product page, the only thing remained is mastering the backend database part, and styling the filter

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,18 @@
 // this file is used to connect to mongodb mongoose
-const mongoose = require('mongoose')
+// Mongoose is a JavaScript object-oriented programming library that creates a connection between MongoDB and the Node.js JavaScript runtime environment.
+const mongoose = require("mongoose");
 
 //every mongoDB thing is async
 const connectDB = async () => {
-    try{
-        const conn = await mongoose.connect(process.env.MONGO_URI)
-        // const conn = await mongoose.connect('mongodb+srv://bradt1234:bradt1234@traversycluster.zz4mm.mongodb.net/mernapp?retryWrites=true&w=majority')
-        console.log(`Hey Firat! MongoDB Connected: ${conn.connection.host}`.cyan.underline);
-    } catch(error){
-        console.log(error);
-        process.exit(1);
-    }
-}
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    // const conn = await mongoose.connect('mongodb+srv://bradt1234:bradt1234@traversycluster.zz4mm.mongodb.net/mernapp?retryWrites=true&w=majority')
+    console.log(
+      `Hey Firat! MongoDB Connected: ${conn.connection.host}`.cyan.underline
+    );
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 module.exports = connectDB;
