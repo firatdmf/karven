48ebf2298d3a8cb5fb0e364f16926abdff519a99
Muhammed Firat Ozturk <firatdmf@gmail.com>
2023-05-30 19:00:44 +0300

    added fabricodepics and initiated product categories section

--- frontend/src/Trial2.jsx
+++ frontend/src/Trial2.jsx
@@ -1,36 +1,8 @@
-import React from "react";
-import classes from "./Trial2.module.css";
-import fabrics from "./fabrics.json";
-import { BsSuitHeart, BsSuitHeartFill } from "react-icons/bs";
+import Products from "./components/Products";
 function Trial2() {
-  let heartIcon;
-  if (5 > 3) {
-    heartIcon = <BsSuitHeart title="Add to favorites" />;
-  } else {
-    heartIcon = <BsSuitHeartFill title="Remove from favorites" />;
-  }
-  let fabric = fabrics[0];
-  const imageDir = "images/kirat/" + fabric.sku + ".jpg";
   return (
     <>
-      <div className={classes.FabricItem}>
-        <div className={classes.card}>
-          <div className={classes.image}>
-            <img src={imageDir} alt={"Image of the fabric: " + fabric.name} />
-          </div>
-          {fabric.theme ? (
-            <div className={classes.theme}>
-              <b>{fabric.theme}</b>
-            </div>
-          ) : (
-            ""
-          )}
-          <div className={classes.productName}>{fabric.name}</div>
-          <div className={classes.SKU}>{fabric.sku}</div>
-          {heartIcon}
-          {/* <div className={classes.favorites}>Add to Favorites</div> */}
-        </div>
-      </div>
+      <Products></Products>
     </>
   );
 }
