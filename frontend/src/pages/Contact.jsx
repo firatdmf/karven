2fddd4a3f31fb461d526101b5a505a3aaf8e3b97
Firat <firatkarven@gmail.com>
2023-08-16 22:11:30 +0300

    comitting from karven pc

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,27 +1,44 @@
 import classes from "./Contact.module.css";
-
+import { Link } from 'react-router-dom'
 function Contact() {
+  // below function takes a string and capitilize the first letter of each word in it
+  const titleCase = function (str) {
+    let splitStr = str.toLowerCase().split(' ');
+    for (let i = 0; i < splitStr.length; i++) {
+      // You do not need to check if i is larger than splitStr length, as your for does that for you
+      // Assign it back to the array
+      splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+    }
+    // Directly return the joined string
+    return splitStr.join(' ');
+  }
   return (
-    <div className={classes.contact}>
-      <div className={classes.firstPart}>
-        <div className={`${classes.map} ${classes.item}`}>
-          <h2>Factory</h2>
+    <div className={classes.Contact}>
+      <div className={`${classes.row} ${classes.row1}`}>
+        <div className={` ${classes.item} ${classes.textInfo}`}>
+          <h2>Manufacturing Plant (Tekirdağ, Türkiye)</h2>
+          <h4>Representative:</h4>
+          <p>Cuma Öztürk</p>
           <h4>Phone:</h4>
           <p>+90 (282) 675-1552 (Office)</p>
           <p>+90 (533) 544-2525 (Mobile)</p>
           <h4>Email:</h4>
-          <p>info@demfirat.com</p>
           <p>karvenmuhasebe@gmail.com</p>
+          <p>info@demfirat.com</p>
+          <h4>Work Hours</h4>
+          <p>24 Hours, All Days</p>
           <h4>Address:</h4>
           <p>
             Karven Tekstil <br />
-            VAKIFLAR OSB MAH D100 CAD NO 38 <br />
-            ERGENE TEKIRDAG, 59930 <br />
-            TURKIYE
+            Vakıflar OSB Mah D100 Cad No 38 <br />
+            Ergene, Tekirdağ 59930 <br />
+            Türkiye
           </p>
+          <h4>Product line:</h4>
+          <p>Drapery, Upholstery, and Bridal Fabrics & Lace Table Runners</p>
         </div>
 
-        <div className={`${classes.item} ${classes.col2}`}>
+        <div className={`${classes.item} ${classes.map} `}>
           <iframe
             title=" "
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2998.686713960122!2d27.633625475834872!3d41.27215670313513!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14b4c1844bb93f27%3A0xcc32597014cd891f!2sDem%20F%C4%B1rat%20Karven%20Tekstil!5e0!3m2!1sen!2sus!4v1692136980792!5m2!1sen!2sus"
@@ -41,8 +58,8 @@ function Contact() {
         </div>
       </div>
 
-      <div className={classes.firstPart}>
-        <div className={`${classes.item} ${classes.col2}`}>
+      <div className={`${classes.row} ${classes.row2}`}>
+        <div className={` ${classes.map} ${classes.item}`}>
           <iframe
             title=" "
             src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d752.6692567557905!2d28.95524754012674!3d41.01044178468319!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1692137913829!5m2!1sen!2sus"
@@ -59,43 +76,54 @@ function Contact() {
             referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
         </div>
-        <div className={`${classes.map} ${classes.item}`}>
-          <h2>Showroom (Curtain Fabric)</h2>
+        <div className={` ${classes.item} ${classes.textInfo}`}>
+          <h2>Fabric Showroom (İstanbul, Türkiye)</h2>
+          <h4>Representative:</h4>
+          <p>Özcan Öztürk</p>
           <h4>Phone:</h4>
-          <p>+90 (282) 675-1552 (Office)</p>
-          <p>+90 (533) 544-2525 (Mobile)</p>
+          <p>+90 (555) 087-5555</p>
           <h4>Email:</h4>
+          <p>krvn.dmf@gmail.com</p>
           <p>info@demfirat.com</p>
-          <p>karvenmuhasebe@gmail.com</p>
+          <h4>Work Hours</h4>
+          <p>Mon - Fri 08:30-19:00, Sat 08:30-14:00 (Istanbul Time)</p>
           <h4>Address:</h4>
           <p>
-            Karven Tekstil <br />
-            VAKIFLAR OSB MAH D100 CAD NO 38 <br />
-            ERGENE TEKIRDAG, 59930 <br />
-            TURKIYE
+            Karven Home Collection <br />
+            Kemalpaşa Mah Gençtürk Cad No 21A <br />
+            Fatih, İstanbul 34134 <br />
+            Türkiye
           </p>
+          <h4>Product line:</h4>
+          <p>Interior Fabrics: Drapery & Upholstery</p>
         </div>
       </div>
 
-      <div className={classes.firstPart}>
-        <div className={`${classes.map} ${classes.item}`}>
-          <h2>Shop (Home Textiles)</h2>
+      <div className={`${classes.row} ${classes.row3}`}>
+        <div className={` ${classes.item} ${classes.textInfo}`}>
+          <h2>Retail Store (İstanbul, Türkiye)</h2>
+          <h4>Representative:</h4>
+          <p>Mustafa Öztürk</p>
           <h4>Phone:</h4>
           <p>+90 (282) 675-1552 (Office)</p>
           <p>+90 (533) 544-2525 (Mobile)</p>
           <h4>Email:</h4>
           <p>info@demfirat.com</p>
-          <p>karvenmuhasebe@gmail.com</p>
+          <p>mustafadmf@hotmail.com</p>
+          <h4>Work Hours</h4>
+          <p>Mon - Fri 09:00-20:30, Sat 09:00-16:00 (Istanbul Time)</p>
           <h4>Address:</h4>
           <p>
-            Karven Tekstil <br />
-            VAKIFLAR OSB MAH D100 CAD NO 38 <br />
-            ERGENE TEKIRDAG, 59930 <br />
-            TURKIYE
+            Demfırat Tekstil <br />
+            Mesihpaşa Mah Hayriye Tüccarı Cad <br />
+            Fatih İstanbul, 34130 <br />
+            Türkiye
           </p>
+          <h4>Product line:</h4>
+          <p>Home Textiles: Bedding, Towels, Bathrobes </p>
         </div>
 
-        <div className={`${classes.item} ${classes.col2}`}>
+        <div className={`${classes.item} ${classes.map} `}>
           <iframe
             title=" "
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3010.82211472467!2d28.95293187582215!3d41.0072665194874!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14cab98864d1f17f%3A0x8bfc30e20c812ba9!2sDem%20F%C4%B1rat%20Karven%20Tekstil!5e0!3m2!1sen!2sus!4v1692138941178!5m2!1sen!2sus"
@@ -117,8 +145,11 @@ function Contact() {
         </div>
       </div>
 
-      <div className={classes.firstPart}>
-        <div className={`${classes.item} ${classes.col2}`}>
+      <div className={`${classes.row} ${classes.row4}`}>
+        <div className={`${classes.item} ${classes.map} `}>
+          <div className={classes.image}>
+            <img src="/images/cover/heimtextile6.jpg" alt="" />
+          </div>
           <iframe
             title=" "
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2248.3406043395717!2d37.69408517663421!3d55.700451096022434!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54ad91d0d5301%3A0x5d8683607752f34!2sCarven%20Wholesale%20Company!5e0!3m2!1sen!2sus!4v1692139145140!5m2!1sen!2sus"
@@ -127,29 +158,38 @@ function Contact() {
             style={{ border: "0" }}
             referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
-          <iframe
+          {/* <iframe
             title=" "
             src="https://www.google.com/maps/embed?pb=!4v1692136743712!6m8!1m7!1sH95SKn0zsoizFxc2esUcNw!2m2!1d41.01049627569196!2d28.95540105451114!3f298.7412353650941!4f-11.65487231269131!5f0.7820865974627469"
             allowFullScreen=""
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
+          ></iframe> */}
         </div>
-        <div className={`${classes.map} ${classes.item}`}>
-          <h2>Warehouse</h2>
+        <div className={`${classes.item} ${classes.textInfo}`}>
+          <h2>Warehouse & Showroom (Moscow, Russia)</h2>
+          <h4>Representative:</h4>
+          <p>Adem Öztürk</p>
           <h4>Phone:</h4>
-          <p>+90 (282) 675-1552 (Office)</p>
-          <p>+90 (533) 544-2525 (Mobile)</p>
+          <p>+7 (968) 738 13 00</p>
+          <p>+7 (916) 055 42 02 </p>
           <h4>Email:</h4>
           <p>info@demfirat.com</p>
-          <p>karvenmuhasebe@gmail.com</p>
+          <p>karventekstil@mail.ru</p>
+          <h4>Website:</h4>
+          <p><a href="https://www.karven.ru" target="_blank" >www.karven.ru</a></p>
+          <h4>Work Hours</h4>
+          <p>Mon - Fri 09:00-18:00, Sat 09:00-16:00 (Moscow Time)</p>
           <h4>Address:</h4>
           <p>
-            Karven Tekstil <br />
-            VAKIFLAR OSB MAH D100 CAD NO 38 <br />
-            ERGENE TEKIRDAG, 59930 <br />
-            Russia
+            г. Москва, 2-й Южнопортовый проезд <br />
+            д.12Г, стр.1 <br />
+            Pоссия
           </p>
+          <p>2nd Yuzhnoportovy proezd, 12G, building 1s <br /> Moscow, Russia</p>
+          <h4>Product line:</h4>
+          <p>{titleCase("bed linen, bedspreads, furniture covers, towels, kitchen towels, towel sets, tablecloths, blankets, pillows, mattress covers, children's assortment, bathrobes, home clothes, sheets, blankets, aprons, sauna sets, ironing board covers.")}</p>
+
         </div>
       </div>
     </div>
